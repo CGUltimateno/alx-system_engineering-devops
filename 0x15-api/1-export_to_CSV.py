b87c564f35ebd14e5cfa773fabca436c8e2dffdb
CGUltimateno <Mohameddeaf65@gmail.com>
2024-01-03 03:13:30 +0200

    yes

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,15 +10,15 @@ import sys
 if __name__ == "__main__":
     """Fetches employee data from an API and saves it to a CSV file"""
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    r = requests.get(url)
-    name = r.json().get('name')
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        user_id)
-    r = requests.get(url)
-    tasks = r.json()
-    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in tasks:
-            writer.writerow([user_id, name, task.get('completed'),
-                            task.get('title')])
+    user_url = ('https://jsonplaceholder.typicode.com/users/{}'
+                .format(user_id))
+    todos_url = ('https://jsonplaceholder.typicode.com/users/{}/todos'
+                 .format(user_id))
+
+    user = requests.get(user_url).json()
+    tasks = requests.get(todos_url).json()
+    with open('{}.csv'.format(user_id), 'w', ) as file:
+        csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        data = [[user_id, user.get('username'), task.get('completed'),
+                 task.get('title')] for task in tasks]
+        csv_writer.writerows(data)
