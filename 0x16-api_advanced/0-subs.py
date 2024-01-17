12c37f9c613364041180ec1efd081df194eeb811
CGU <Mohameddeaf65@gmail.com>
2024-01-17 17:52:06 +0200

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,22 @@
 """"
     Queries the Reddit API and returns the number of subscribers
     """
-import requests
-import response
+from sys import argv
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """"
         Returns the number of subscribers
         """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user = {"User-Agent": "CGUltimateno"}
-    request = requests.get(url, headers=user, allow_redirects=False).json()
-    if response.status_code == 404:
+    user = {"User-Agent": "Mozilla/5.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = get(url, headers=user, allow_redirects=False)
+    try:
+        return response.json().get("data").get("subscribers")
+    except Exception:
         return 0
-    return request.get('data').get('subscribers')
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
