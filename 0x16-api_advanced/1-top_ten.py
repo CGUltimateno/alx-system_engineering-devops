cf76ec67970d8ffde758bcf3e56a74d30dcae519
CGU <Mohameddeaf65@gmail.com>
2024-01-17 17:59:46 +0200

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,16 @@
+#!/usr/bin/python3
+""" Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+"""
 from requests import get
 
 
 def top_ten(subreddit):
     """ Prints the titles of the first 10 hot posts for a given subreddit. """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-
-    with get(url, allow_redirects=False) as res:
+    with get(f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+             allow_redirects=False) as res:
         if res.status_code == 200:
-            try:
-                data = res.json()
-                for item in data["data"]["children"]:
-                    print(item["data"]["title"])
-            except ValueError as e:
-                print(f"Error decoding JSON: {e}")
+            for item in res.json()["data"]["children"]:
+                print(item["data"]["title"])
         else:
             print("None")
