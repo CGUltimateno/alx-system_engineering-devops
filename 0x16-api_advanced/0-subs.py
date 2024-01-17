eeacf5f3428dd977094da43a71b4a0e6270bc6c9
CGU <Mohameddeaf65@gmail.com>
2024-01-17 18:06:08 +0200

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,14 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers """
     with requests.get("https://www.reddit.com/r/{}/about.json".
-                      format(subreddit),
+                              format(subreddit),
                       headers={"User-Agent": "Custom_user"},
                       allow_redirects=False) as res:
         if res.status_code == 200:
             return res.json()["data"]["subscribers"]
         else:
             return 0
+
 
 if __name__ == "__main__":
     print(number_of_subscribers(argv[1]))
